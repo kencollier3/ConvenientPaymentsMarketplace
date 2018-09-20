7586d8d15844628bd388db18c4d5381ca90c0377
Ken Collier <mckennoncollier@gmail.com>
2018-09-19 20:22:21 -0600

    Moves nonsensitive code repo from VSTS to public GitHub

---
+++ js/featured-item.js
@@ -0,0 +1,37 @@
+var itemCount = 0;
+const MAX_ITEMS = 3;
+
+for(itemCount = 0; itemCount < MAX_ITEMS; itemCount++){
+    // <div class="row featuredContainer">
+    //                     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 joomla plugins component">
+    //                         <div class="single-item-grid">
+    //                             <div class="item-img">
+    //                                 <a href="https://www.jackhenry.com" target="_blank"><img src="img/product/jackhenry.jpg" alt="jackhenry.com" class="img-responsive" /></a>
+    //                                 <div class="trending-sign" data-tips="Trending"><i class="fa fa-bolt" aria-hidden="true"></i></div>
+    //                             </div>
+    //                             <div class="item-content">
+    //                                 <div class="item-info">
+    //                                     <h3><a href="https://www.jackhenry.com" target="_blank">Jack Henry</a></h3>
+    //                                     <span>Joomla Component</span>
+    //                                     <div class="price">$15</div>
+    //                                 </div>
+    //                                 <div class="item-profile">
+    //                                     <div class="profile-title">
+    //                                         <div class="img-wrapper"><img src="img/profile/1.jpg" alt="profile" class="img-responsive img-circle"></div>
+    //                                         <span>PsdBosS</span>
+    //                                     </div>
+    //                                     <div class="profile-rating">
+    //                                         <ul>
+    //                                             <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    //                                             <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    //                                             <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    //                                             <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    //                                             <li><i class="fa fa-star" aria-hidden="true"></i></li>
+    //                                             <li>(<span> 05</span> )</li>
+    //                                         </ul>
+    //                                     </div>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+}
